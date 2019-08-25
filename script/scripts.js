2911a3df8949c260778da2077c977ae4a8ae51b7
Carlos Ramirez <ramirezcarc06@gmail.com>
2019-08-25 09:58:34 -0400

    bootstrap bugs fixed

--- script/scripts.js
+++ script/scripts.js
@@ -7,16 +7,19 @@ $('document').ready(function(){
 		bottom: "30px",
 		opacity: 1
 	}, 1000);
-
-	// Text appear when user scrolls down
-
+	// Effects when user scrolls down
 	$trigger_text = 0;
 	$trigger_grid = 0;
+	$trigger_planBox = 0;
 
 	$(window).scroll(function(){
 		let y_Position = window.pageYOffset;
 		let text_position = 350;
 		let grid_position = 500;
+		let planBox_position = 3700;
+
+		// text on first section
+
 		if(y_Position > text_position && $trigger_text == 0){
 
 			$('.content-text > h2, .content-text > p').animate({
@@ -25,34 +28,24 @@ $('document').ready(function(){
 
 			$trigger_text = 1; //Make sure it doesn't run again.
 		}
+
+		// grid items 
+
 		if(y_Position > grid_position && $trigger_grid == 0){
 			let i;
-			console.log("hola");
-			$(".item-1").animate({
-  					opacity:1
-  			}, 1000);
-  			$(".item-2").animate({
-  					opacity:1
-  			}, 1000);
-  			$(".item-3").animate({
-  					opacity:1
-  			}, 100);
-  			$(".item-4").animate({
-  					opacity:1
-  			}, 100);
-  			$(".item-5").animate({
-  					opacity:1
-  			}, 1000);
-  			$(".item-6").animate({
-  					opacity:1
-  			}, 1000);
-  			$(".item-7").animate({
-  					opacity:1
-  			}, 1000);
+			// the same effect is applied to every single grid item
+			// but they occur at a different time
+			for(i = 1; i < 8; i++){
+				$('.item-'+i).delay(i * 200).animate({
+					opacity: 1
+				});
+			}
 
 			$trigger_grid = 1; //Make sure it doesn't run again.
 		}
 
-
+		if(y_Position > planBox_position && $trigger_planBox == 0){
+			$('#popular').addClass('shadow-lg');
+		}
 	})
 });
